72be2a4cb6299178155b4cdff8ca06dbf07bda92
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-03-11 08:07:27 +0800

    update import paths

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import CartItem from './CartItem';
+import CartItem from 'CartItem';
 import CartTotalContainer from '../containers/CartTotalContainer';
 import PropTypes from 'prop-types';
 
