2ada6dbe1b5dd7d08583feeda7a1394fed658071
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-03-11 08:11:41 +0800

    update index.css path

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './configureStore';
-import 'index.css';
+import './index.css';
 import App from './containers/App';
 // import registerServiceWorker from './registerServiceWorker';
 
