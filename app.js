22f1e9810a4b905dec0fda1d666ce2e4a8e80e8a
Jen <30688276+helplah@users.noreply.github.com>
2019-03-12 15:23:33 +0800

    add asterisk to app.get

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use('/auth', authRoutes);
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 }); 
 
